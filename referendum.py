ab67dd544711295bebf7645b02eb7f046f3d784b
kaa090 <kaa090@gmail.com>
2022-05-19 21:43:46 +0300

    add log

--- referendum.py
+++ referendum.py
@@ -2,6 +2,7 @@
 import logging
 import datetime
 import math
+import sqlite3
 from aiogram import Bot, Dispatcher, executor, types
 from aiogram.utils.callback_data import CallbackData
 from aiogram.utils.exceptions import MessageNotModified
@@ -9,11 +10,16 @@ from aiogram.utils.markdown import escape_md
 from contextlib import suppress
 from config import TOKEN
 
+FILE_LOG = "referendums.log"
+FILE_DB = "referendums.db"
+
 class MyBot:
 	def __init__(self):
-		self.referendums = {}
+		logging.basicConfig(level = logging.DEBUG, 
+							format = "[%(asctime)s] [%(levelname)-8s] [%(funcName)s]: %(message)s",
+							handlers = [logging.FileHandler(FILE_LOG), logging.StreamHandler()])
 
-		logging.basicConfig(level = logging.INFO)
+		self.referendums = {}
 		self.bot = Bot(token = TOKEN)
 		self.dp = Dispatcher(self.bot)
 		self.dp.register_message_handler(self.cmd_create, commands = "create")
@@ -24,20 +30,28 @@ class MyBot:
 
 	async def cmd_create(self, message: types.Message):
 		args = message.get_args().split("|")
-		self.args = args[1:]
-		self.chat_members = await message.chat.get_member_count()
 		self.first_voters_count = int(args[0])
+
+		self.args = []
+		for arg in args[1:]:
+			self.args.append(escape_md(arg))
+
+		self.chat_members = await message.chat.get_member_count()
 		self.init_referendum()
-		print(f"created: {message.chat.id} {datetime.datetime.now().isoformat(' ', 'seconds')}")
 		msg = self.update_message(0, '')
 		keyboard = self.get_keyboard()
+		
+		logging.info(self.args)
+		logging.info(f"[chatID={message.chat.id}, msgID={message.message_id}] vote created: {self.referendums}")
 		
 		await message.answer(msg, reply_markup = keyboard, parse_mode="MarkdownV2")
 
 	async def process_callback(self, cbq: types.CallbackQuery, callback_data: dict):
 		msg = self.update_message(int(callback_data["button"]), cbq.from_user)
 		keyboard = self.get_keyboard()
-		print(f"chatID={cbq.message.chat.id}, {datetime.datetime.now().isoformat(' ', 'seconds')}. {cbq.from_user} pressed button {callback_data['button']}")
+		
+		logging.info(f"[chatID={cbq.message.chat.id}, msgID={cbq.message.message_id}] {cbq.from_user} pressed button {callback_data['button']}")
+		
 		with suppress(MessageNotModified):
 			await cbq.message.edit_text(msg, reply_markup = keyboard, parse_mode="MarkdownV2")
 		await cbq.answer()
@@ -66,6 +80,8 @@ class MyBot:
 		votes_total = 0
 		votes_percent = 0
 		votes_percent_by_chat = 0
+
+		logging.debug(f"start: {self.referendums}")
 
 		if button:
 			mention = f"[{escape_md(user.first_name)}](tg://user?id={user.id})"
@@ -125,7 +141,7 @@ class MyBot:
 
 				userlist.append(f"{mark1}[{escape_md(usr.first_name)}](tg://user?id={usr.id}){mark2}")
 				current_user += 1
-######################################################				
+			
 			if(userlist):
 				msg += ", ".join(userlist) + '\n\n'
 			else:
@@ -135,6 +151,8 @@ class MyBot:
 			votes_percent_by_chat = int(100 * round(votes_total/(self.chat_members-1), 2))
 		msg += f"👥 {votes_total} of {self.chat_members - 1} \\({votes_percent_by_chat}%\\) people voted so far\\."
 		
+		logging.debug(f"msg={msg}")
+		logging.debug(f"end: {self.referendums}")
 		return msg
 
 if __name__ == "__main__":
