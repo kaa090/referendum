38fda63d0fadfc25f8ea11fa338410779b2be55c
kaa090 <49017533+kaa090@users.noreply.github.com>
2022-08-05 16:02:46 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+import logging
+
+TOKEN = "5322293349:AAHjYqXIE8DHsb3tvinBDRgvdPXyTt838Rs" # БОТЯРА
+# TOKEN = "5355907600:AAF0pGLWh1YbVBEyJS5uXjVPxLZuQ0YvrBk" # Т-1000
+FILE_LOG = "referendums.log"
+FILE_DB = "referendums.db"
+FILE_README = "readme.txt"
+FILE_HELP = "help.txt"
+LEVEL = logging.INFO	#restart if changed. LEVEL = [DEBUG, INFO, WARNING, ERROR, CRITICAL]
+
+RFR_GAME = 0
+RFR_SINGLE = 1
+RFR_MULTI = 2
+RFR_GAME2 = 3
+
+BUTTON_ID_YES = 1
+BUTTON_ID_ADD = 4
+BUTTON_ID_DEL = 5
+BUTTON_ID_OPT = 6
